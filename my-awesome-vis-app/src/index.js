2c485ee58296b89916e47b78f219b342ba223f2e
nitin jain <nitin.e@gmail.com>
2020-06-01 23:54:45 +0530

    index js updated with appolo api

--- my-awesome-vis-app/src/index.js
+++ my-awesome-vis-app/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+});
 
 ReactDOM.render(
   <React.StrictMode>
